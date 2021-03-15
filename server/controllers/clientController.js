83d4fdbd8b7c3510f2e2555b5ad1dd6bd4bae23e
pmg23_b.kondev <boriskondev@gmail.com>
2021-03-15 22:41:16 +0200

    15.03.2021

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -3,12 +3,12 @@ const User = require("../models/user");
 const mongoose = require("mongoose");
 
 const clientController = {
-    // Almost good to go for the Weekly status page.
-    // Is there a way to populate only projects with status "active"?
     findAllClients: async (req, res) => {
+        // const filter = { clientName: { $eq: "Visa" } }; This shit works!
         const filter = { status: "active" };
         const allClients = await Client
             .find(filter)
+            .lean()
             .populate({
                 path: "projects",
                 select: { "projectName": 1, "tasks": 1, "_id": 0 },
@@ -17,10 +17,12 @@ const clientController = {
                     select: { "taskName": 1, "startDate": 1, "endDate": 1,  "responsible": 1, "_id": 0 },
                     populate: {
                         path: "responsible",
+                        match: { _id: "604e776fbe4a5b41f0834477" },
                         select: { "username": 1, "_id": 0 }
                     }
                 }
             });
+
         res.json(allClients);
     },
 
