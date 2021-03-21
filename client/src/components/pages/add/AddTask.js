5dfff23a7b4cda656bd786ea90cb706685f83ec4
pmg23_b.kondev <boriskondev@gmail.com>
2021-03-21 17:31:40 +0200

    21.03.2021

--- client/src/components/pages/add/AddTask.js
+++ client/src/components/pages/add/AddTask.js
@@ -1,32 +1,64 @@
 import Header from "../../common/Header";
 import "./AddTask.css";
+import {useState} from "react";
+import { clientsOptions, projectsOptions, responsible } from "./sampleData"
 
 const AddTask = () => {
+    const [taskName, setTaskName] = useState("");
+    const [clientName, setClientName] = useState("");
+    const [projectName, setProjectName] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [peopleResponsible, setPeopleResponsible] = useState([]);
+
+    const filterProjectsOptions = () => {
+        // https://www.geeksforgeeks.org/how-to-change-a-selects-options-based-on-another-dropdown-using-react/
+    }
+
+    let newTask = {taskName, clientName, projectName, startDate, endDate, peopleResponsible}
+
+    console.log(newTask);
+
     return (
         <>
             <Header title="Добави задача"/>
+
             <form className="create-task-form">
 
                 <div className="form-field">
-                    <label htmlFor="task-name">Име</label>
-                    <input type="text" name="task-name" id="task-name" autoComplete="off" required autoFocus />
+                    <label>Име</label>
+                    <input
+                        type="text"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
+                        autoComplete="off"
+                        autoFocus
+                        required
+                    />
                 </div>
 
                 <div className="form-field">
-                    <label htmlFor="client-name">Клиент</label>
-                    <select name="client-name" id="client-name" required>
-                        <option value="" selected="selected" hidden="hidden">Избери</option>
-                        <option value="visa">Visa</option>
-                        <option value="kamenitza">Kamenitza</option>
-                        <option value="hp">HP</option>
-                        <option value="coca-cola">Coca-Cola</option>
+                    <label>Клиент</label>
+                    <select
+                        onChange={(e) => setClientName(e.target.value)}
+                        required
+                    >
+                        <option hidden>Избери</option>
+                        {clientsOptions.map(option => (
+                            <option key={option.id} value={option.id}>{option.label}</option>
+                        ))}
+
                     </select>
                 </div>
 
                 <div className="form-field">
-                    <label htmlFor="project-name">Проект</label>
-                    <select name="project-name" id="project-name" required>
-                        <option value="" selected="selected" hidden="hidden">Избери</option>
+                    <label>Проект</label>
+                    <select
+                        onChange={(e) => setProjectName(e.target.value)}
+                        required
+                    >
+
+                        <option hidden="hidden">Все още хардкоднати - търси се решение</option>
                         <option value="bansko-branding">Bansko branding</option>
                         <option value="sprint-promotion">Spring promotion</option>
                         <option value="emag">EMAG</option>
@@ -35,28 +67,40 @@ const AddTask = () => {
                 </div>
 
                 <div className="form-field dates">
-                    <label htmlFor="task-term">Срок</label>
+                    <label>Срок</label>
                     <div id="task-term">
-                        <input type="date" name="start" id="today" required />
+                        <input
+                            type="date"
+                            onChange={(e) => setStartDate(e.target.value)}
+                            required
+                        />
                         <span>-</span>
-                        <input type="date" name="end" required />
+                        <input
+                            type="date"
+                            onChange={(e) => setEndDate(e.target.value)}
+                            required
+                        />
                     </div>
                 </div>
 
                 <div className="form-field">
-                    <label htmlFor="persons-responsible">Отговорни</label>
-                    <select name="persons-responsible" id="persons-responsible" size="11" multiple required>
-                        <option value="1">Боби</option>
-                        <option value="2">Веско</option>
-                        <option value="3">Данчо</option>
-                        <option value="4">Диляна</option>
-                        <option value="5">Катя</option>
-                        <option value="6">Крис</option>
-                        <option value="7">Марто</option>
-                        <option value="8">Милена</option>
-                        <option value="9">Соня</option>
-                        <option value="10">Стефи</option>
-                        <option value="11">Хилда</option>
+                    <label>Отговорни</label>
+                    <select
+                        onClick={(e) => setPeopleResponsible(responsible => new Set([...responsible, e.target.value]))}
+                        size={responsible.length}
+                        multiple
+                        required
+                    >
+
+                        {responsible.map(option => (
+                            <option
+                                key={option.id}
+                                value={option.id}
+                            >
+                                {option.name}
+                            </option>
+                        ))}
+
                     </select>
                 </div>
 
---
+++ client/src/components/pages/add/sampleData.js
@@ -0,0 +1,36 @@
+export const clientsOptions = [
+    {label: "Visa", id: 1},
+    {label: "Kamenitza", id: 2},
+    {label: "HP", id: 3},
+    {label: "Big bank", id: 4},
+    {label: "Nvidia", id: 5},
+    {label: "OMC1", id: 6},
+];
+
+export const projectsOptions = [
+    {id: 1, projects: ["Spring promotion", "Mass transit", "Bansko branding", "Urgent stats"]},
+    {id: 2, projects: ["RGB", "Airport branding", "Events pitch", "Production"]},
+    {id: 3, projects: ["Leaflets design", "Sales conference", "National promo stores"]},
+    {id: 4, projects: ["Revolut API", "East coast expansion"]},
+    {id: 5, projects: ["New GTX9000", "Doom eternal bundle calls", "Secret projects"]},
+    {id: 6, projects: ["New milk strain", "Paris showroom preparation", "Events for escherichia coli"]},
+]
+
+export const responsible = [
+    {id: 1, name: "Боби"},
+    {id: 2, name: "Веско"},
+    {id: 3, name: "Данчо"},
+    {id: 4, name: "Диляна"},
+    {id: 5, name: "Катя"},
+    {id: 6, name: "Крис"},
+    {id: 7, name: "Марто"},
+    {id: 8, name: "Милена"},
+    {id: 9, name: "Соня"},
+    {id: 10, name: "Стефи"},
+    {id: 11, name: "Хилда"},
+    {id: 12, name: "Ивел"},
+    {id: 13, name: "Хамуд"},
+    {id: 14, name: "Герда"},
+    {id: 15, name: "Костил"},
+    {id: 16, name: "Хибрид"},
+]
