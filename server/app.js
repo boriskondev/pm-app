09e6900ae41051957a64ac9d32d13ab7a4c8d354
pmg23_b.kondev <boriskondev@gmail.com>
2021-03-26 00:39:05 +0200

    26.03.2021

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 // https://www.positronx.io/react-mern-stack-crud-app-tutorial/
 
+const port = 4000;
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -15,19 +17,17 @@ const dbname = "pm-app";
 const dbURI = `mongodb+srv://${user}:${pass}@cluster0.d5skl.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log("MongoDB Atlas connected."))
+    .then(() => app.listen(port, () => {
+        console.log(`The server is ready and listening on port ${port}.`)
+    }))
     .catch((err) => console.log("Could not connect to MongoDB Atlas: " + err));
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/", allRoutes);
 
-const port = 4000;
-
-app.listen(port, () => {
-    console.log(`The server is ready and listening on port ${port}.`)
-});
 
--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -1,53 +1,56 @@
 const Client = require("../models/client");
 
+// https://www.restapitutorial.com/httpstatuscodes.html
+
 const clientController = {
     findAllClients: async (req, res) => {
-        // const filter = { clientName: { $eq: "Visa" } }; This shit works!
-        const filter = { status: "active" };
-        // const allClients = await Client
-        //     .find(filter)
-            // .lean()
-            // .select({ "clientName": 1, "_id": 1 })
-
-        // res.json(allClients);
-
-        const allClients = await Client
-            .find(filter)
-            .lean()
-            .populate({
-                path: "projects",
-                select: { "projectName": 1, "tasks": 1, "_id": 1 },
-                populate: {
-                    path: "tasks",
-                    select: { "taskName": 1, "startDate": 1, "endDate": 1,  "responsible": 1, "_id": 0 },
+        try {
+            // const filter = { clientName: { $eq: "Visa" } }; This shit works!
+            const filter = { status: "active" };
+            const allClients = await Client
+                .find(filter)
+                .lean()
+                .populate({
+                    path: "projects",
+                    select: { "projectName": 1, "tasks": 1, "_id": 1 },
                     populate: {
-                        path: "responsible",
-                        match: { _id: "604e776fbe4a5b41f0834477" },
-                        select: { "username": 1, "_id": 0 }
+                        path: "tasks",
+                        select: { "taskName": 1, "startDate": 1, "endDate": 1,  "responsible": 1, "_id": 0 },
+                        populate: {
+                            path: "responsible",
+                            match: { _id: "604e776fbe4a5b41f0834477" },
+                            select: { "username": 1, "_id": 0 }
+                        }
                     }
-                }
-            });
+                });
 
-        res.json(allClients);
+            res.status(200).json(allClients);
+        } catch (error) {
+            res.status(404).json({ message: error.message });
+        }
+
     },
 
     createClient: async (req, res) => {
         const {clientName, createdBy} = req.body;
         const newClient = new Client({clientName, createdBy});
-        const savedClient = await newClient.save();
-        const message = `${clientName} successfully created!`
-        res.json(message);
+
+        try {
+            await newClient.save();
+            res.status(200).json(newClient);
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+        }
     },
 
     findClient: async (req, res) => {
         try {
             const id = req.params.id;
             const foundClient = await Client.findById(id);
-            res.json(foundClient);
-        } catch (err) {
-            res.json("Client does not exist. Try again.");
+            res.status(200).json(foundClient);
+        } catch (error) {
+            res.status(404).json({ message: error.message });
         }
-
     },
 }
 
