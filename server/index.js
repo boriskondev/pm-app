cf06f2c40228296a3c48073f67dd0e444ae0f42c
pmg23_b.kondev <boriskondev@gmail.com>
2021-03-26 12:58:20 +0200

    26.03.2021

--- server/index.js
+++ server/index.js
@@ -1,21 +1,18 @@
 // https://www.positronx.io/react-mern-stack-crud-app-tutorial/
 
-const port = 4000;
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const database = process.env.DB_URI;
 
 const allRoutes = require("./routes/allRoutes");
 
-const user = "boriskondev";
-const pass = "boriskondev123";
-const dbname = "pm-app";
-
-const dbURI = `mongodb+srv://${user}:${pass}@cluster0.d5skl.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log("MongoDB Atlas connected."))
     .then(() => app.listen(port, () => {
         console.log(`The server is ready and listening on port ${port}.`)
