773b44e1733b4438743581d18e41fe37bda794f4
pmg23_b.kondev <boriskondev@gmail.com>
2021-03-27 23:37:50 +0200

    27.03.2021

--- client/src/components/pages/status/Homepage.js
+++ client/src/components/pages/status/Homepage.js
@@ -3,25 +3,28 @@ import "./Homepage.css";
 import Header from "../../common/Header";
 import {Link} from "react-router-dom";
 import {MdEmail as Email, MdPalette as Palette, MdAssessment as Assessment, MdSort as Sort} from "react-icons/md";
+import endpoints from "../../../services/api";
 
 class Homepage extends Component {
     constructor() {
         super();
         this.state = {
-            peopleData: [
-                {id: 1, path: "/", name: "Хилда", tasks: 30, projects: 9, clients: 3, department: "management"},
-                {id: 2, path: "/", name: "Соня", tasks: 12, projects: 18, clients: 1, department: "creative"},
-                {id: 3, path: "/", name: "Боби", tasks: 25, projects: 13, clients: 2, department: "clientService"},
-                {id: 4, path: "/", name: "Милена", tasks: 22, projects: 33, clients: 22, department: "management"},
-                {id: 5, path: "/", name: "Веско", tasks: 11, projects: 23, clients: 22, department: "creative"},
-                {id: 6, path: "/", name: "Даран", tasks: 0, projects: 3, clients: 1, department: "clientService"},
-                {id: 7, path: "/", name: "Елхо", tasks: 1, projects: 1, clients: 2, department: "creative"},
-                {id: 8, path: "/", name: "Зевс", tasks: 225, projects: 213, clients: 122, department: "clientService"}
-            ],
-            weeklyData: {totalTasks: 101, totalProjects: 55, totalClients: 13}
+            usersData: "",
+            activeTasks: ""
         }
     }
 
+    async componentDidMount() {
+        Promise.all([
+            fetch(endpoints.USERS).then(response => response.json()),
+            fetch(endpoints.TASKS).then(response => response.json())
+        ]).then(([allUsersInDB, allTasksInDB]) => {
+            this.setState({
+                usersData: allUsersInDB.filter(user => user.tasks.length > 0),
+                activeTasks: allTasksInDB.length,
+            });
+        })
+    }
 
     render() {
 
@@ -31,16 +34,6 @@ class Homepage extends Component {
             clientService: <Email size={25}/>,
             sort: <Sort size={20}/>
         }
-
-        const rowsData = this.state.peopleData.map(person => (
-            <tr key={person.id}>
-                <td><a href={person.path}>{person.name}</a></td>
-                <td>{person.tasks}</td>
-                <td>{person.projects}</td>
-                <td>{person.clients}</td>
-                <td className="icon">{icons[person.department]}</td>
-            </tr>
-        ));
 
         return (
             <>
@@ -52,20 +45,27 @@ class Homepage extends Component {
                         <tr>
                             <th>{icons.sort}</th>
                             <th>Задачи</th>
-                            <th>Проекти</th>
-                            <th>Клиенти</th>
                             <th>{icons.sort}</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {rowsData}
+                        {/*{!this.state.usersData && (*/}
+                        {/*    <p>Малко търпение :)</p>*/}
+                        {/*)}*/}
+                        {this.state.usersData && (
+                            this.state.usersData.map(person => (
+                                <tr key={person._id}>
+                                    <td><a href={person._id}>{person.username}</a></td>
+                                    <td>{person.tasks.length}</td>
+                                    <td className="icon">{icons[person.department]}</td>
+                                </tr>
+                            ))
+                        )}
                         </tbody>
                         <tfoot>
                         <tr>
                             <td></td>
-                            <td>{this.state.weeklyData.totalTasks}</td>
-                            <td>{this.state.weeklyData.totalProjects}</td>
-                            <td>{this.state.weeklyData.totalClients}</td>
+                            <td>{this.state.activeTasks.length}</td>
                             <td></td>
                         </tr>
                         </tfoot>
