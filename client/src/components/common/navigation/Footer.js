67114db5e4a75f4d8fec238b78c75d7da60364c3
pmg23_b.kondev <boriskondev@gmail.com>
2021-05-21 18:27:45 +0300

    21.05.2021

--- client/src/components/common/navigation/Footer.js
+++ client/src/components/common/navigation/Footer.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./Footer.css"
 import {Link} from "react-router-dom";
-import SearchClients from "../../pages/search/SearchClients";
+import {useContext} from "react";
+import AuthContext from "../../../context/AuthContext";
 
 function Footer() {
+    const {loggedIn} = useContext(AuthContext);
 
     return (
         <footer>
             <img src={process.env.PUBLIC_URL + "/images/logos/footer-logo.png"} alt="footer-logo"/>
-            <div className="statistics">
-                <p>Search</p>
-                <ul>
-                    <li><Link to="/search-clients">Clients</Link></li>
-                    {/*<li><Link>Projects</Link></li>*/}
-                    {/*<li><Link>Tasks</Link></li>*/}
-                </ul>
-            </div>
+
+            {loggedIn === true && (
+                <div className="statistics">
+                    <p>Search</p>
+                    <ul>
+                        <li><Link to="/search-clients">Clients</Link></li>
+                        {/*<li><Link>Projects</Link></li>*/}
+                        {/*<li><Link>Tasks</Link></li>*/}
+                    </ul>
+                </div>
+            )}
+
         </footer>
     )
 }
