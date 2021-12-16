0da61620274a6757b55bbece2b5250d464d60eb3
bkondev <boriskondev@gmail.com>
2021-12-16 13:10:36 +0200

    Improved styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
